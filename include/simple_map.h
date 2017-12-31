a4e8bb8b10017ba9206a9cfba8e709101487622c
user <user@localhost>
2017-12-31 16:59:52 +0300

    code cleanup
    unused declarations are remuved

--- include/simple_map.h
+++ include/simple_map.h
@@ -1,9 +1,6 @@
 #ifndef SIMPLE_MAP_H_
 #define SIMPLE_MAP_H_
 
-int map_init(int max_map_size, int max_value_size);
-void map_dispose();
-
 int map_contain(char *key);
 char* map_put(char *key);
 char* map_get(char *key);
