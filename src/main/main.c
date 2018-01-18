96a7f47a7f6ad8ceb347317dbd07444e9b5178ca
vsolovey <509145+vsolovey@users.noreply.github.com>
2018-01-19 00:40:32 +0300

    #2 Sometime substring is not highlighted

--- src/main/main.c
+++ src/main/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 #include "common.h"
 #include "colors.h"
 #include "cmd_line_args.h"
 #include "highlight.h"
 
-int BUF_LEN = 256;
+
+void get_buf(char **buf, size_t *len, int new_len) {
+	int new_buf_len = new_len * 21;
+	if (new_buf_len > *len) {
+		*buf = (char*)realloc(*buf, new_buf_len);
+	}
+}
 
 /*
    args: { -s substring [-c color] | -w word [-c color] } | -f config_file | -h
@@ -15,20 +22,25 @@ int BUF_LEN = 256;
 */
 void main(int argc, char **argv) {
 	colors_init();
-	char rdbuf[BUF_LEN];
-	char wrbuf[BUF_LEN*21];
 
 	char *data[2][DATA_MAX_LEN];
 	int data_len = 0;
 	int ok = parse_args(argv[0], argv+1, argc-1, data, &data_len);
 
 	if (ok == TRUE) {
-		fgets(rdbuf, BUF_LEN, stdin);
+		size_t rd_len = 0;
+		char *rdbuf = NULL;
+        int stdread = getline(&rdbuf, &rd_len, stdin);
+		size_t wr_len = 0;
+		char *wrbuf = NULL;
+		get_buf(&wrbuf, &wr_len, rd_len);
 		while (feof(stdin) == 0) {
 			int read = strlen(rdbuf);
 			int write = emphase_line(rdbuf, read, wrbuf, data, data_len);
 			int written = fwrite(wrbuf, 1, write, stdout);
-			fgets(rdbuf, BUF_LEN, stdin);
+			stdread = getline(&rdbuf, &rd_len, stdin);
+			get_buf(&wrbuf, &wr_len, rd_len);
 		}
+		free(wrbuf);
 	}
 }
